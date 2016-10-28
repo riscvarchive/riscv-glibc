f02ec12b5e5945b9fe6e4f46fe48f13a66dd0b97
Andrew Waterman <waterman@cs.berkeley.edu>
2016-10-28 14:21:01 -0700

    Correct contributor information

--- sysdeps/riscv/atomic-machine.h
+++ sysdeps/riscv/atomic-machine.h
@@ -1,7 +1,7 @@
 /* Low-level functions for atomic operations. RISC-V version.
    Copyright (C) 2011-2016 Free Software Foundation, Inc.
 
-   Contributed by Andrew Waterman (waterman@cs.berkeley.edu) at UC Berkeley.
+   Contributed by Andrew Waterman (andrew@sifive.com).
 
    This file is part of the GNU C Library.
 
