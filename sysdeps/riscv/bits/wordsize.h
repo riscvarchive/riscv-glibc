36cc08205d020ce3d53e335176da880328c2b5dd
Andrew Waterman <andrew@sifive.com>
2016-12-09 13:58:24 -0800

    Fix RV32

--- sysdeps/riscv/bits/wordsize.h
+++ sysdeps/riscv/bits/wordsize.h
@@ -24,4 +24,8 @@
 
 #if __riscv_xlen == 64
 # define __WORDSIZE_TIME64_COMPAT32 1
+#else
+# define __WORDSIZE_TIME64_COMPAT32 0
+# define __WORDSIZE32_SIZE_ULONG    0
+# define __WORDSIZE32_PTRDIFF_LONG  0
 #endif
