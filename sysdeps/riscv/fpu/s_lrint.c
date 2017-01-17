348ad0883799ebca1b101c4e0be68eaf32360db9
Andrew Waterman <andrew@sifive.com>
2017-01-16 19:40:50 -0800

    Add lots more rounding functions

---
+++ sysdeps/riscv/fpu/s_lrint.c
@@ -0,0 +1,41 @@
+/* Copyright (C) 2017 Free Software Foundation, Inc.
+   Contributed by Andrew Waterman (andrew@sifive.com).
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#if __riscv_flen >= 64
+
+#ifndef __LP64__
+
+#include <math.h>
+
+long
+__lrint (double x)
+{
+  long res;
+  asm ("fcvt.w.d %0, %1" : "=r" (res) : "f" (x));
+  return res;
+}
+
+weak_alias (__lrint, lrint)
+
+#endif
+
+#else
+
+#include <sysdeps/ieee754/dbl-64/s_lrint.c>
+
+#endif
