7442cfc257427a09a5f52e913a0c126b0e4371b7
Andrew Waterman <andrew@sifive.com>
2017-01-31 19:53:30 -0800

    Only use aligned register pairs for varargs
    https://github.com/riscv/riscv-elf-psabi-doc/pull/15

--- sysdeps/unix/sysv/linux/riscv/kernel-features.h
+++ sysdeps/unix/sysv/linux/riscv/kernel-features.h
@@ -20,9 +20,3 @@
 #define __ASSUME_SENDMMSG_SYSCALL	1
 
 #include_next <kernel-features.h>
-
-/* Define this if your 32-bit syscall API requires 64-bit register
-   pairs to start with an even-number register.  */
-#if __riscv_xlen == 32
-# define __ASSUME_ALIGNED_REGISTER_PAIRS	1
-#endif
