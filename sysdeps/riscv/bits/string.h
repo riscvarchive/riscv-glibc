7621e2023cc3a147e44f08042bb429b3ebd8598f
Andrew Waterman <andrew@sifive.com>
2017-04-05 18:31:49 -0700

    inline memcpy

--- sysdeps/riscv/bits/string.h
+++ sysdeps/riscv/bits/string.h
@@ -10,9 +10,17 @@
 
 #define _STRING_INLINE_unaligned   0
 
-#if defined(__GNUC__) && !defined(__cplusplus)
+#if defined(__GNUC__) && !defined(__NO_STRING_INLINES)
 
-static __inline__ unsigned long __libc_detect_null(unsigned long w)
+#ifndef __STRING_INLINE
+# ifndef __extern_inline
+#  define __STRING_INLINE inline
+# else
+#  define __STRING_INLINE __extern_inline
+# endif
+#endif
+
+__STRING_INLINE unsigned long __libc_detect_null(unsigned long w)
 {
   unsigned long mask = 0x7f7f7f7f;
   if (sizeof(long) == 8)
@@ -20,6 +28,56 @@ static __inline__ unsigned long __libc_detect_null(unsigned long w)
   return ~(((w & mask) + mask) | w | mask);
 }
 
-#endif /* __GNUC__ && !__cplusplus */
+#define __aligned_to_const_size(k, p, n)				\
+  (__extension__ (__builtin_constant_p (n)				\
+		  && __alignof__(*(p)) >= (k)				\
+		  && (n) % (k) == 0))
+
+#define __memcpy_max_ops 8
+
+#define __use_memcpy_c(t, d, s, n)					\
+  (__aligned_to_const_size(sizeof (t), d, n)				\
+   && __aligned_to_const_size(sizeof (t), s, n)				\
+   && (n) / sizeof (t) <= __memcpy_max_ops)
+
+#define __declare_memcpy_c(t)						\
+  __STRING_INLINE void *__memcpy_c_##t (void *__restrict __d,		\
+				     const void *__restrict __s,	\
+				     size_t __n)			\
+  {									\
+    t *__td = (t *)__d;							\
+    const t *__ts = (const t *)__s;					\
+    switch ((__n) / sizeof (t))						\
+      {									\
+      case 8: *__td++ = *__ts++;					\
+      case 7: *__td++ = *__ts++;					\
+      case 6: *__td++ = *__ts++;					\
+      case 5: *__td++ = *__ts++;					\
+      case 4: *__td++ = *__ts++;					\
+      case 3: *__td++ = *__ts++;					\
+      case 2: *__td++ = *__ts++;					\
+      case 1: *__td++ = *__ts++;					\
+      case __memcpy_max_ops != 8: break;				\
+      }									\
+    return __d;								\
+  }
+
+#if defined(__OPTIMIZE__) && !defined(__OPTIMIZE_SIZE__)
+
+#pragma GCC push_options
+#pragma GCC ("funroll-loops")
+__declare_memcpy_c(long)
+__declare_memcpy_c(int)
+#pragma GCC pop_options
+
+#define _HAVE_STRING_ARCH_memcpy 1
+#define memcpy(d, s, n)							\
+  (__use_memcpy_c (long, d, s, n) ? __memcpy_c_long (d, s, n) :		\
+   __use_memcpy_c (int, d, s, n) ? __memcpy_c_int (d, s, n) :		\
+   memcpy(d, s, n))
+
+#endif /* __OPTIMIZE__ && !__OPTIMIZE_SIZE__ */
+
+#endif /* __GNUC__ */
 
 #endif /* bits/string.h */
