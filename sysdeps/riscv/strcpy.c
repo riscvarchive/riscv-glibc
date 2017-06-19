2e5279c039c479a7088147fb1578f559d73db172
Andrew Waterman <andrew@sifive.com>
2017-06-19 15:43:31 -0700

    Remove bits/string.h, add missing copyright notices

--- sysdeps/riscv/strcpy.c
+++ sysdeps/riscv/strcpy.c
@@ -1,5 +1,23 @@
+/* Copyright (C) 2011-2017 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
 #include <string.h>
 #include <stdint.h>
+#include "detect_null_byte.h"
 
 #undef strcpy
 
@@ -13,7 +31,7 @@ char* strcpy(char* dst, const char* src)
     long* ldst = (long*)dst;
     const long* lsrc = (const long*)src;
 
-    while (!__libc_detect_null(*lsrc))
+    while (!detect_null_byte(*lsrc))
       *ldst++ = *lsrc++;
 
     dst = (char*)ldst;
