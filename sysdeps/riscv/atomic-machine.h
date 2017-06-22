707ad3d4fa947aa45d21de3166b3a8fcb710a572
Darius Rad <darius@bluespec.com>
2017-06-22 19:26:04 -0400

    Use compiler built-in functions to implement atomic macros, where possible.

--- sysdeps/riscv/atomic-machine.h
+++ sysdeps/riscv/atomic-machine.h
@@ -40,6 +40,127 @@ typedef uintmax_t uatomic_max_t;
 #define USE_ATOMIC_COMPILER_BUILTINS 1
 #define ATOMIC_EXCHANGE_USES_CAS 0
 
+/* Compare and exchange.
+   For all "bool" routines, we return FALSE if exchange succesful.  */
+
+# define __arch_compare_and_exchange_bool_8_int(mem, newval, oldval, model) \
+  ({									\
+    typeof (*mem) __oldval = (oldval);					\
+    !__atomic_compare_exchange_n (mem, (void *) &__oldval, newval, 0,	\
+				  model, __ATOMIC_RELAXED);		\
+  })
+
+# define __arch_compare_and_exchange_bool_16_int(mem, newval, oldval, model) \
+  ({									\
+    typeof (*mem) __oldval = (oldval);					\
+    !__atomic_compare_exchange_n (mem, (void *) &__oldval, newval, 0,	\
+				  model, __ATOMIC_RELAXED);		\
+  })
+
+# define __arch_compare_and_exchange_bool_32_int(mem, newval, oldval, model) \
+  ({									\
+    typeof (*mem) __oldval = (oldval);					\
+    !__atomic_compare_exchange_n (mem, (void *) &__oldval, newval, 0,	\
+				  model, __ATOMIC_RELAXED);		\
+  })
+
+#  define __arch_compare_and_exchange_bool_64_int(mem, newval, oldval, model) \
+  ({									\
+    typeof (*mem) __oldval = (oldval);					\
+    !__atomic_compare_exchange_n (mem, (void *) &__oldval, newval, 0,	\
+				  model, __ATOMIC_RELAXED);		\
+  })
+
+# define __arch_compare_and_exchange_val_8_int(mem, newval, oldval, model) \
+  ({									\
+    typeof (*mem) __oldval = (oldval);					\
+    __atomic_compare_exchange_n (mem, (void *) &__oldval, newval, 0,	\
+				 model, __ATOMIC_RELAXED);		\
+    __oldval;								\
+  })
+
+# define __arch_compare_and_exchange_val_16_int(mem, newval, oldval, model) \
+  ({									\
+    typeof (*mem) __oldval = (oldval);					\
+    __atomic_compare_exchange_n (mem, (void *) &__oldval, newval, 0,	\
+				 model, __ATOMIC_RELAXED);		\
+    __oldval;								\
+  })
+
+# define __arch_compare_and_exchange_val_32_int(mem, newval, oldval, model) \
+  ({									\
+    typeof (*mem) __oldval = (oldval);					\
+    __atomic_compare_exchange_n (mem, (void *) &__oldval, newval, 0,	\
+				 model, __ATOMIC_RELAXED);		\
+    __oldval;								\
+  })
+
+#  define __arch_compare_and_exchange_val_64_int(mem, newval, oldval, model) \
+  ({									\
+    typeof (*mem) __oldval = (oldval);					\
+    __atomic_compare_exchange_n (mem, (void *) &__oldval, newval, 0,	\
+				 model, __ATOMIC_RELAXED);		\
+    __oldval;								\
+  })
+
+/* Atomic compare and exchange. */
+
+# define atomic_compare_and_exchange_bool_acq(mem, new, old)	\
+  __atomic_bool_bysize (__arch_compare_and_exchange_bool, int,	\
+			mem, new, old, __ATOMIC_ACQUIRE)
+
+# define atomic_compare_and_exchange_val_acq(mem, new, old)	\
+  __atomic_val_bysize (__arch_compare_and_exchange_val, int,	\
+		       mem, new, old, __ATOMIC_ACQUIRE)
+
+# define atomic_compare_and_exchange_val_rel(mem, new, old)	 \
+  __atomic_val_bysize (__arch_compare_and_exchange_val, int,    \
+                       mem, new, old, __ATOMIC_RELEASE)
+
+/* Atomic exchange (without compare).  */
+
+# define __arch_exchange_8_int(mem, newval, model)	\
+  __atomic_exchange_n (mem, newval, model)
+
+# define __arch_exchange_16_int(mem, newval, model)	\
+  __atomic_exchange_n (mem, newval, model)
+
+# define __arch_exchange_32_int(mem, newval, model)	\
+  __atomic_exchange_n (mem, newval, model)
+
+#  define __arch_exchange_64_int(mem, newval, model)	\
+  __atomic_exchange_n (mem, newval, model)
+
+# define atomic_exchange_acq(mem, value)				\
+  __atomic_val_bysize (__arch_exchange, int, mem, value, __ATOMIC_ACQUIRE)
+
+# define atomic_exchange_rel(mem, value)				\
+  __atomic_val_bysize (__arch_exchange, int, mem, value, __ATOMIC_RELEASE)
+
+/* Atomically add value and return the previous (unincremented) value.  */
+
+# define __arch_exchange_and_add_8_int(mem, value, model)	\
+  __atomic_fetch_add (mem, value, model)
+
+# define __arch_exchange_and_add_16_int(mem, value, model)	\
+  __atomic_fetch_add (mem, value, model)
+
+# define __arch_exchange_and_add_32_int(mem, value, model)	\
+  __atomic_fetch_add (mem, value, model)
+
+#  define __arch_exchange_and_add_64_int(mem, value, model)	\
+  __atomic_fetch_add (mem, value, model)
+
+# define atomic_exchange_and_add_acq(mem, value)			\
+  __atomic_val_bysize (__arch_exchange_and_add, int, mem, value,	\
+		       __ATOMIC_ACQUIRE)
+
+# define atomic_exchange_and_add_rel(mem, value)			\
+  __atomic_val_bysize (__arch_exchange_and_add, int, mem, value,	\
+		       __ATOMIC_RELEASE)
+
+/* Miscellaneous. */
+
 #define asm_amo(which, ordering, mem, value) ({ 		\
   typeof(*mem) __tmp; 						\
   if (sizeof(__tmp) == 4)					\
@@ -53,48 +174,6 @@ typedef uintmax_t uatomic_max_t;
   else								\
     abort();							\
   __tmp; })
-
-/* Atomic compare and exchange. */
-
-#define atomic_cas(ordering, mem, newval, oldval) ({ 		\
-  typeof(*mem) __tmp; 						\
-  int __tmp2; 							\
-  if (sizeof(__tmp) == 4)					\
-    asm volatile ("1: lr.w" ordering "\t%0, %2\n"		\
-		  "bne\t%0, %z4, 1f\n"				\
-		  "sc.w" ordering "\t%1, %z3, %2\n"		\
-		  "bnez\t%1, 1b\n"				\
-		  "1:"						\
-		  : "=&r"(__tmp), "=&r"(__tmp2), "+A"(*(mem))	\
-		  : "rJ"(newval), "rJ"(oldval));		\
-  else if (sizeof(__tmp) == 8)					\
-    asm volatile ("1: lr.d" ordering "\t%0, %2\n"		\
-		  "bne\t%0, %z4, 1f\n"				\
-		  "sc.d" ordering "\t%1, %z3, %2\n"		\
-		  "bnez\t%1, 1b\n"				\
-		  "1:"						\
-		  : "=&r"(__tmp), "=&r"(__tmp2), "+A"(*(mem))	\
-		  : "rJ"(newval), "rJ"(oldval));		\
-  else								\
-    abort();							\
-  __tmp; })
-
-#define atomic_compare_and_exchange_val_acq(mem, newval, oldval) \
-  atomic_cas(".aq", mem, newval, oldval)
-
-#define atomic_compare_and_exchange_val_rel(mem, newval, oldval) \
-  atomic_cas(".rl", mem, newval, oldval)
-
-/* Atomic exchange (without compare).  */
-
-#define atomic_exchange_acq(mem, value) asm_amo("amoswap", ".aq", mem, value)
-#define atomic_exchange_rel(mem, value) asm_amo("amoswap", ".rl", mem, value)
-
-
-/* Atomically add value and return the previous (unincremented) value.  */
-
-#define atomic_exchange_and_add_acq(mem, value) asm_amo("amoadd", ".aq", mem, value)
-#define atomic_exchange_and_add_rel(mem, value) asm_amo("amoadd", ".rl", mem, value)
 
 #define atomic_max(mem, value) asm_amo("amomaxu", ".aq", mem, value)
 #define atomic_min(mem, value) asm_amo("amominu", ".aq", mem, value)
