cb76c85451b65cf95f80f734e3c4082aa058b9f0
Palmer Dabbelt <palmer@dabbelt.com>
2017-12-13 18:41:48 -0800

    Match the Linux sigcontext API in glibc

--- sysdeps/unix/sysv/linux/riscv/sys/ucontext.h
+++ sysdeps/unix/sysv/linux/riscv/sys/ucontext.h
@@ -25,6 +25,7 @@
 
 #include <bits/types/sigset_t.h>
 #include <bits/types/stack_t.h>
+#include <asm/ptrace.h>
 
 #ifdef __USE_MISC
 # define __ctx(fld) fld
@@ -53,21 +54,73 @@ typedef greg_t gregset_t[NGREG];
 typedef union __riscv_fp_state fpregset_t;
 
 #endif
+
+/* These structures all must match what's in Linux.  Some are copied.  */
+struct __riscv_gp_state {
+  unsigned long pc;
+  unsigned long ra;
+  unsigned long sp;
+  unsigned long gp;
+  unsigned long tp;
+  unsigned long t0;
+  unsigned long t1;
+  unsigned long t2;
+  unsigned long s0;
+  unsigned long s1;
+  unsigned long a0;
+  unsigned long a1;
+  unsigned long a2;
+  unsigned long a3;
+  unsigned long a4;
+  unsigned long a5;
+  unsigned long a6;
+  unsigned long a7;
+  unsigned long s2;
+  unsigned long s3;
+  unsigned long s4;
+  unsigned long s5;
+  unsigned long s6;
+  unsigned long s7;
+  unsigned long s8;
+  unsigned long s9;
+  unsigned long s10;
+  unsigned long s11;
+  unsigned long t3;
+  unsigned long t4;
+  unsigned long t5;
+  unsigned long t6;
+};
+
 typedef struct mcontext_t
   {
-    /* gregs[0] holds the program counter. */
-    unsigned long __ctx(gregs)[32];
-    unsigned long long __ctx(fpregs)[66] __attribute__((aligned(16)));
+    union {
+      struct __riscv_gp_state byname;
+      unsigned long byindex[32];
+    } gregs;
+    union  __riscv_fp_state fpregs;
   } mcontext_t;
 
 /* Userlevel context.  */
 typedef struct ucontext_t
   {
-    unsigned long int uc_flags;
-    struct ucontext_t *uc_link;
-    stack_t uc_stack;
+    unsigned long     uc_flags;
+    struct ucontext  *uc_link;
+    stack_t           uc_stack;
+    sigset_t          uc_sigmask;
+    /* There's some padding here to allow sigset_t to be expanded in the
+     * future.  Though this is unlikely, other architectures put uc_sigmask
+     * at the end of this structure and explicitly state it can be
+     * expanded, so we didn't want to box ourselves in here. */
+    __u8              __unused[1024 / 8 - sizeof(sigset_t)];
+    /* We can't put uc_sigmask at the end of this structure because we need
+     * to be able to expand sigcontext in the future.  For example, the
+     * vector ISA extension will almost certainly add ISA state.  We want
+     * to ensure all user-visible ISA state can be saved and restored via a
+     * ucontext, so we're putting this at the end in order to allow for
+     * infinite extensibility.  Since we know this will be extended and we
+     * assume sigset_t won't be extended an extreme amount, we're
+     * prioritizing this. */
     mcontext_t uc_mcontext;
-    sigset_t uc_sigmask;
   } ucontext_t;
 
 #endif /* sys/ucontext.h */
