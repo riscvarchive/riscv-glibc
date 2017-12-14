689debb0b819ac670bb8abca515dc8fefedb848e
Palmer Dabbelt <palmer@dabbelt.com>
2017-12-13 16:11:10 -0800

    fixups

--- sysdeps/unix/sysv/linux/riscv/rv64/jmp_buf-macros.h
+++ sysdeps/unix/sysv/linux/riscv/rv64/jmp_buf-macros.h
@@ -1,6 +1,25 @@
+/*
+ * Produced by this program
+ *
+ * #include <stdio.h>
+ * #include <unistd.h>
+ * #include <setjmp.h>
+ * #include <stddef.h>
+ * 
+ * int main(int argc, char **argv)
+ * {
+ *     printf("#define JMP_BUF_SIZE %d\n", sizeof(jmp_buf));
+ *     printf("#define JMP_BUF_ALIGN %d\n", __alignof__(jmp_buf));
+ *     printf("#define SIGJMP_BUF_SIZE %d\n", sizeof(sigjmp_buf));
+ *     printf("#define SIGJMP_BUF_ALIGN %d\n", __alignof__(sigjmp_buf));
+ *     printf("#define MASK_WAS_SAVED_OFFSET %d\n", offsetof(struct __jmp_buf_tag, __mask_was_saved));
+ *     printf("#define SAVED_MASK_OFFSET %d\n", offsetof(struct __jmp_buf_tag, __saved_mask));
+ * }
+ */
+
 #define JMP_BUF_SIZE 344
-#define SIGJMP_BUF_SIZE 344
 #define JMP_BUF_ALIGN 8
+#define SIGJMP_BUF_SIZE 344
 #define SIGJMP_BUF_ALIGN 8
 #define MASK_WAS_SAVED_OFFSET 208
 #define SAVED_MASK_OFFSET 216
