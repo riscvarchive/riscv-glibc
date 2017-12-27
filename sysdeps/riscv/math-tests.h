94e48722c675127fbc2be415aa057ff9cdaab381
Palmer Dabbelt <palmer@dabbelt.com>
2017-12-26 20:51:52 -0800

    RISC-V: Generic <math.h> and soft-fp Routines
    This patch contains the miscellaneous math routines and headers we have
    implemented for RISC-V.  This includes things from <math.h> that aren't
    completely ISA-generic, floating-point bit manipulation, and soft-fp
    hooks.
    
    FIXME: add ieee745/soft-fp via a riscv/soft-fp directory.

---
+++ sysdeps/riscv/math-tests.h
@@ -0,0 +1,30 @@
+/* Configuration for math tests.  RISC-V version
+   Copyright (C) 2014-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+   Copied from the aarch64 version
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+/* Trapping exceptions are not supported on RISC-V.  */
+#define EXCEPTION_ENABLE_SUPPORTED(EXCEPT)	((EXCEPT) == 0)
+
+#define EXCEPTION_TESTS_float 0
+#define EXCEPTION_TESTS_double        0
+#define EXCEPTION_TESTS_long_double   0
+
+/* RISC-V floating-point instructions do not preserve NaN payloads.  */
+#define SNAN_TESTS_PRESERVE_PAYLOAD	0
+
+#include_next <math-tests.h>
