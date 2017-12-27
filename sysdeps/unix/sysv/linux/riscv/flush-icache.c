e41191dbda3f72425bdba41d396116eda348692e
Palmer Dabbelt <palmer@dabbelt.com>
2017-12-26 20:52:27 -0800

    RISC-V: Linux ABI
    Linux-specific code that is required for maintaining ABI compatibility.
    This doesn't contain the actual system call interface, that is split out
    in order to avoid having a patch that's too big.

---
+++ sysdeps/unix/sysv/linux/riscv/flush-icache.c
@@ -0,0 +1,61 @@
+/* RISC-V instruction cache flushing VDSO calls
+   Copyright (C) 2017 Free Software Foundation, Inc.
+
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of the
+   License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <dl-vdso.h>
+#include <stdlib.h>
+#include <atomic.h>
+#include <sys/cachectl.h>
+
+typedef int (*func_type) (void *, void *, unsigned long);
+
+static func_type
+__lookup_riscv_flush_icache (void)
+{
+  PREPARE_VERSION_KNOWN (linux_version, LINUX_4_15);
+
+  func_type func = _dl_vdso_vsym ("__vdso_flush_icache", &linux_version);
+
+  /* The vDSO is required, as there is no exposed system call equivalent.  */
+  if (!func)
+    abort ();
+
+  return func;
+}
+
+#ifdef SHARED
+
+# define INIT_ARCH()
+libc_ifunc (__riscv_flush_icache, __lookup_riscv_flush_icache ())
+
+#else
+
+int
+__riscv_flush_icache (void *start, void *end, unsigned long flags)
+{
+  static volatile func_type cached_func;
+
+  func_type func = cached_func;
+
+  if (!func)
+    cached_func = func = __lookup_riscv_flush_icache ();
+
+  return func (start, end, flags);
+}
+
+#endif
---
+++ sysdeps/unix/sysv/linux/riscv/sys/ucontext.h
@@ -0,0 +1,118 @@
+/* struct ucontext definition, RISC-V version.
+   Copyright (C) 1997-2017 Free Software Foundation, Inc.  
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <http://www.gnu.org/licenses/>.  */
+
+/* Don't rely on this, the interface is currently messed up and may need to
+   be broken to be fixed.  */
+#ifndef _SYS_UCONTEXT_H
+#define _SYS_UCONTEXT_H	1
+
+#include <features.h>
+
+#include <bits/types/sigset_t.h>
+#include <bits/types/stack_t.h>
+
+#ifdef __USE_MISC
+# define __ctx(fld) fld
+#else
+# define __ctx(fld) __ ## fld
+#endif
+
+#ifdef __USE_MISC
+# define NGREG	32
+
+# define REG_PC 0
+# define REG_RA 1
+# define REG_SP 2
+# define REG_TP 4
+# define REG_S0 8
+# define REG_A0 10
+# define REG_NARGS 8
+
+typedef unsigned long greg_t;
+
+/* Container for all general registers.  */
+typedef greg_t gregset_t[NGREG];
+
+/* Container for floating-point state.  */
+typedef union __riscv_fp_state fpregset_t;
+#endif
+
+struct __riscv_f_ext_state
+  {
+    unsigned int __ctx(f)[32];
+    unsigned int __ctx(fcsr);
+  };
+
+struct __riscv_d_ext_state
+  {
+    unsigned long long __ctx(f[32]);
+    unsigned int __ctx(fcsr);
+  };
+
+struct __riscv_q_ext_state
+  {
+    unsigned long long __ctx(f[64]) __attribute__ ((__aligned__ (16)));
+    unsigned int __ctx(fcsr);
+    /*
+     * Reserved for expansion of sigcontext structure.  Currently zeroed
+     * upon signal, and must be zero upon sigreturn.
+     */
+    unsigned int __ctx(reserved)[3];
+  };
+
+union __riscv_fp_state
+  {
+    struct __riscv_f_ext_state __ctx(f);
+    struct __riscv_d_ext_state __ctx(d);
+    struct __riscv_q_ext_state __ctx(q);
+  };
+
+typedef unsigned long __riscv_gp_state[32];
+
+typedef struct mcontext_t
+  {
+    __riscv_gp_state __ctx(gregs);
+    union  __riscv_fp_state __ctx(fpregs);
+  } mcontext_t;
+
+/* Userlevel context.  */
+typedef struct ucontext_t
+  {
+    unsigned long      __ctx(uc_flags);
+    struct ucontext_t *uc_link;
+    stack_t            uc_stack;
+    sigset_t           uc_sigmask;
+    /* There's some padding here to allow sigset_t to be expanded in the
+     * future.  Though this is unlikely, other architectures put uc_sigmask
+     * at the end of this structure and explicitly state it can be
+     * expanded, so we didn't want to box ourselves in here. */
+    char               __unused[1024 / 8 - sizeof (sigset_t)];
+    /* We can't put uc_sigmask at the end of this structure because we need
+     * to be able to expand sigcontext in the future.  For example, the
+     * vector ISA extension will almost certainly add ISA state.  We want
+     * to ensure all user-visible ISA state can be saved and restored via a
+     * ucontext, so we're putting this at the end in order to allow for
+     * infinite extensibility.  Since we know this will be extended and we
+     * assume sigset_t won't be extended an extreme amount, we're
+     * prioritizing this. */
+    mcontext_t uc_mcontext;
+  } ucontext_t;
+
+#undef __ctx
+
+#endif /* sys/ucontext.h */
