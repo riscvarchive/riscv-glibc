6b448a0d4761a6d35cdb9f139428aa7068107d68
Darius Rad <darius@bluespec.com>
2018-01-02 13:25:06 -0500

    Use __glibc_likely/unlikely instead of __builtin_expect for boolean.

--- sysdeps/unix/sysv/linux/riscv/sysdep.h
+++ sysdeps/unix/sysv/linux/riscv/sysdep.h
@@ -130,7 +130,7 @@
 # define INLINE_SYSCALL(name, nr, args...)				\
   ({ INTERNAL_SYSCALL_DECL (err);					\
      long __sys_result = INTERNAL_SYSCALL (name, err, nr, args);	\
-     if (__builtin_expect (INTERNAL_SYSCALL_ERROR_P (__sys_result, ), 0)) \
+     if (__glibc_unlikely (INTERNAL_SYSCALL_ERROR_P (__sys_result, ))) \
        {								\
          __set_errno (INTERNAL_SYSCALL_ERRNO (__sys_result, ));		\
 	 __sys_result = (unsigned long) -1;				\
