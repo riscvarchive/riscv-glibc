3d9ee95da4874483d729344a0f0dd3b3565f6021
Palmer Dabbelt <palmer@dabbelt.com>
2018-01-29 07:31:06 -0800

    Add missing space

--- sysdeps/riscv/sys/asm.h
+++ sysdeps/riscv/sys/asm.h
@@ -26,7 +26,7 @@
 # define REG_S sd
 # define REG_L ld
 #elif __riscv_xlen == 32
-#error "rv32i-based targets are not supported"
+# error "rv32i-based targets are not supported"
 #else
 # error __riscv_xlen must equal 32 or 64
 #endif
